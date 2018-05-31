4dda53bc396b9cbe84d0ebc3084ee7a4fc6f2a41
Abdul Nasir <abdulnasarba@gmail.com>
2018-05-31 11:09:14 +0400

    final
    final release

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -15,12 +15,12 @@ $(document).ready(function(){
     }
   });
 
-  var myWorks = [{"img": "assets/img/portfolio/port7.png", "link": "https://github.com/NiranjanRavichandran", "name": "mHealth"},
-  {"img": "assets/img/portfolio/port8.png", "link": "https://itunes.apple.com/us/app/mauritius-attractions/id1122309065?mt=8", "name": "Mauritius Attractions"},
-  {"img": "assets/img/portfolio/port1.png", "link": "http://meraenglish.com", "name": "Meraenglish"},
-  {"img": "assets/img/portfolio/port2.png", "link": "https://itunes.apple.com/us/app/mauritius-explored/id1012328297?mt=8", "name": "Mauritius Explored"},
-  {"img": "assets/img/portfolio/port3.png", "link": "https://github.com/NiranjanRavichandran/MauritiusExp-2.0", "name": "iCook"},
-  {"img": "assets/img/portfolio/port4.png", "link": "https://itunes.apple.com/us/app/auto-meter-plus/id817389494?mt=8", "name": "Auto Meter Plus"}];
+  var myWorks = [{"img": "assets/img/portfolio/project1.png", "link": "https://itunes.apple.com/in/app/yano/id1362470923?mt=8", "name": "Yano"},
+  {"img": "assets/img/portfolio/project2.png", "link": "https://itunes.apple.com/in/app/iptikar-express/id1357965607?mt=8", "name": "Iptikar Express"},
+  {"img": "assets/img/portfolio/project3.png", "link": "https://itunes.apple.com/in/app/st-mobile/id1254962696?mt=8", "name": "ST-Mobile"},
+  {"img": "assets/img/portfolio/project4.png", "link": "https://itunes.apple.com/in/app/smart-e-asset/id1216073041?mt=8", "name": "Smart e-Asset"},
+				 {"img": "assets/img/portfolio/project5.png", "link": "https://itunes.apple.com/in/app/%D8%AA%D8%B7%D8%A8%D9%8A%D9%82-%D8%A7%D9%84%D9%85%D9%82%D8%A7%D9%84/id1260322809?mt=8", "name": "تطبيق المقال"},
+ {"img": "assets/img/portfolio/project6.png", "link": "https://itunes.apple.com/in/app/pay-alaan/id1222547185?mt=8", "name": "Pay Alaan"}];
   populateMyWorks(myWorks);
 
 });
@@ -30,7 +30,7 @@ function populateMyWorks(myWorks) {
   for(i=0; i <myWorks.length; i++){
     var object = myWorks[i];
     var row = document.createElement('div');
-      row.className = 'col-md-4 no_padding';
+	  row.className = 'col-md-4 anba329';
       row.innerHTML = '<div class="single_image"><img src="'+object['img']+'" alt=""> <div class="image_overlay"> <a href="'+object['link']+'">View Project</a><h2>'+object['name']+'</h2></div>';
     //document.getElementById('recentWorks').appendChild(row);
       $(row).hide().appendTo('#recentWorks').fadeIn(1000);
@@ -38,8 +38,17 @@ function populateMyWorks(myWorks) {
 }
 
 function loadMoreWorks() {
-  var myWorks = [{"img": "assets/img/portfolio/port5.png", "link": "http://revathykumar.in", "name": "Revathy Kumar"},
-  {"img": "assets/img/portfolio/port6.png", "link": "http://meraenglish.com", "name": "Meraenglish"}];
+	var myWorks = [{"img": "assets/img/portfolio/project10.png", "link": "https://itunes.apple.com/ca/app/zho/id961537327?mt=8", "name": "ZHO"},
+				   {"img": "assets/img/portfolio/project12.png", "link": "https://itunes.apple.com/in/app/power-fix/id1355484307?mt=8", "name": "Power Fix"},
+				   {"img": "assets/img/portfolio/project8.png", "link": "https://itunes.apple.com/in/app/federal-transport-authority-uae/id969171572?mt=8", "name": "FTA UAE"},
+				   {"img": "assets/img/portfolio/project9.png", "link": "http://smartv.ae/index.php/visitor-management-system/", "name": "VMS"}, {"img": "assets/img/portfolio/project7.png", "link": "https://www.uasg.ae/", "name": "UASG"},
+				   {"img": "assets/img/portfolio/project11.png", "link": "http://www.alaan.co.uk/i_doc_app.html", "name": "iDoc"},
+				   {"img": "assets/img/portfolio/project13.png", "link": "http://www.app-store.es/bzycar", "name": "bzycar"},
+				   {"img": "assets/img/portfolio/project14.png", "link": "https://www.youtube.com/watch?v=wBhovi_0ZT8", "name": "PromayarnDaily"},
+				   {"img": "assets/img/portfolio/project15.png", "link": "https://www.facebook.com/Promayarn-158972500908889/", "name": "Promayarn"},
+				   {"img": "assets/img/portfolio/project16.png", "link": "assets/img/portfolio/project16.png", "name": "Promayarn"},
+				   {"img": "assets/img/portfolio/project17.png", "link": "http://www.compass-softwares.com/iphone-and-mobile-application-developers-in-thailand", "name": "CRM SALE"},
+				   {"img": "assets/img/portfolio/project17a.png", "link": "assets/img/portfolio/project17a.png", "name": "iVerify"}];
   populateMyWorks(myWorks);
   var x = document.getElementById('loadButton')
   x.parentNode.removeChild(x);
